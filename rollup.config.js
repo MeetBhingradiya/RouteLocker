46e1f6d835cf07d449ba3617f2280193a7459cbf
MEET BHINGRADIYA <meetbhingradiya36@gmail.com>
2022-12-11 09:40:42 +0530

    RL Template init 2

--- rollup.config.js
+++ rollup.config.js
@@ -9,52 +9,52 @@ import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
 
 export default {
-  input: `src/index.tsx`,
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-      sourcemap: true,
-      plugins: [terser()],
-      exports: 'auto',
+    input: `src/index.tsx`,
+    output: [
+        {
+            file: pkg.main,
+            format: 'cjs',
+            sourcemap: true,
+            plugins: [terser()],
+            exports: 'auto',
+        },
+        {
+            file: pkg.module,
+            format: 'es',
+            sourcemap: true,
+            plugins: [terser()],
+            exports: 'auto',
+        },
+        {
+            file: 'dist/index.js',
+            format: 'cjs',
+            sourcemap: true,
+            exports: 'auto',
+        },
+    ],
+    watch: {
+        include: 'src/**',
     },
-    {
-      file: pkg.module,
-      format: 'es',
-      sourcemap: true,
-      plugins: [terser()],
-      exports: 'auto',
-    },
-    {
-      file: 'dist/index.js',
-      format: 'cjs',
-      sourcemap: true,
-      exports: 'auto',
-    },
-  ],
-  watch: {
-    include: 'src/**',
-  },
-  plugins: [
-    external(),
-    postcss({
-      modules: true,
-    }),
-    // Allow json resolution
-    json(),
-    // Compile TypeScript files
-    typescript({
-      useTsconfigDeclarationDir: true,
-      exclude: ['**/__tests__/**', '*.spec.*', '*.test.*'],
-      clean: true,
-    }),
-    // Allow node_modules resolution, so you can use 'external' to control
-    // which external modules to include in the bundle
-    // https://github.com/rollup/rollup-plugin-node-resolve#usage
-    resolve(),
-    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-    commonjs(),
-    // Resolve source maps to the original source
-    sourceMaps(),
-  ],
+    plugins: [
+        external(),
+        postcss({
+            modules: true,
+        }),
+        // Allow json resolution
+        json(),
+        // Compile TypeScript files
+        typescript({
+            useTsconfigDeclarationDir: true,
+            exclude: ['**/__tests__/**', '*.spec.*', '*.test.*'],
+            clean: true,
+        }),
+        // Allow node_modules resolution, so you can use 'external' to control
+        // which external modules to include in the bundle
+        // https://github.com/rollup/rollup-plugin-node-resolve#usage
+        resolve(),
+        // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
+        commonjs(),
+        // Resolve source maps to the original source
+        sourceMaps(),
+    ],
 };
--- tools/init.ts
+++ tools/init.ts
@@ -11,12 +11,12 @@
 
 const { execSync } = require('child_process');
 const {
-  rmdirSync,
-  rmSync,
-  existsSync,
-  lstatSync,
-  readdirSync,
-  unlinkSync,
+    rmdirSync,
+    rmSync,
+    existsSync,
+    lstatSync,
+    readdirSync,
+    unlinkSync,
 } = require('fs');
 
 /* eslint-disable @typescript-eslint/no-var-requires */
@@ -24,8 +24,8 @@ const {
 
 // eslint-disable-next-line dot-notation
 if (process.env['RTL_SKIP_POSTINSTALL']) {
-  console.log('Skipping post-install process...');
-  process.exit(0);
+    console.log('Skipping post-install process...');
+    process.exit(0);
 }
 
 const { basename, resolve } = require('path');
@@ -35,49 +35,49 @@ const { cyan, green, red, underline, yellow } = require('colors');
 const _prompt = require('prompt');
 
 function deleteFolderRecursive(path: any) {
-  if (existsSync(path)) {
-    const files = readdirSync(path);
-    files.forEach(function (file: any) {
-      const curPath = path + '/' + file;
-      if (lstatSync(curPath).isDirectory()) {
-        // recurse
-        deleteFolderRecursive(curPath);
-      } else {
-        // delete file
-        unlinkSync(curPath);
-      }
-    });
-    rmdirSync(path);
-  }
+    if (existsSync(path)) {
+        const files = readdirSync(path);
+        files.forEach(function (file: any) {
+            const curPath = path + '/' + file;
+            if (lstatSync(curPath).isDirectory()) {
+                // recurse
+                deleteFolderRecursive(curPath);
+            } else {
+                // delete file
+                unlinkSync(curPath);
+            }
+        });
+        rmdirSync(path);
+    }
 }
 
 // Note: These should all be relative to the project root directory
 const rmDirs = ['.git', 'tools', 'CHANGELOG.md'];
 const rmFiles = ['.all-contributorsrc', '.gitattributes'];
 const modifyFiles = [
-  'LICENSE',
-  'package.json',
-  'README.md',
-  'CODE_OF_CONDUCT.md',
-  'shell.nix',
-  '.github/workflows/release-please.yml',
+    'LICENSE',
+    'package.json',
+    'README.md',
+    'CODE_OF_CONDUCT.md',
+    'shell.nix',
+    '.github/workflows/release-please.yml',
 ];
 
 /**
  * Removes items from the project that aren't needed after the initial setup
  */
 function removeItems() {
-  console.log(underline.white('Removed'));
+    console.log(underline.white('Removed'));
 
-  // The directories and files are combined here, to simplify the function,
-  // as the 'rm' command checks the item type before attempting to remove it
-  const rmItems = rmDirs.concat(rmFiles);
+    // The directories and files are combined here, to simplify the function,
+    // as the 'rm' command checks the item type before attempting to remove it
+    const rmItems = rmDirs.concat(rmFiles);
 
-  rmItems
-    .map((f) => resolve(__dirname, '..', f))
-    .forEach((p) => deleteFolderRecursive(p));
-  console.log(red(rmItems.join('\n')));
-  console.log('\n');
+    rmItems
+        .map((f) => resolve(__dirname, '..', f))
+        .forEach((p) => deleteFolderRecursive(p));
+    console.log(red(rmItems.join('\n')));
+    console.log('\n');
 }
 
 /**
@@ -88,77 +88,77 @@ function removeItems() {
  * @param usermail
  */
 function modifyContents(
-  libraryName: string,
-  username: string,
-  usermail: string
+    libraryName: string,
+    username: string,
+    usermail: string
 ) {
-  console.log(underline.white('Modified'));
+    console.log(underline.white('Modified'));
 
-  const files = modifyFiles.map((f) => resolve(__dirname, '..', f));
-  try {
-    replace.sync({
-      files,
-      from: [
-        /--libraryname--/g,
-        /--username--/g,
-        /--usermail--/g,
-        /--currentYear--/g,
-      ],
-      to: [libraryName, username, usermail, new Date().getFullYear()],
-    });
-    console.log(yellow(modifyFiles.join('\n')));
-  } catch (error) {
-    console.error('An error occurred modifying the file: ', error);
-  }
-  console.log('\n');
+    const files = modifyFiles.map((f) => resolve(__dirname, '..', f));
+    try {
+        replace.sync({
+            files,
+            from: [
+                /--libraryname--/g,
+                /--username--/g,
+                /--usermail--/g,
+                /--currentYear--/g,
+            ],
+            to: [libraryName, username, usermail, new Date().getFullYear()],
+        });
+        console.log(yellow(modifyFiles.join('\n')));
+    } catch (error) {
+        console.error('An error occurred modifying the file: ', error);
+    }
+    console.log('\n');
 }
 
 function modifyGitignoreFile(): void {
-  const files = ['.gitignore'].map((f) => resolve(__dirname, '..', f));
-  replace.sync({
-    files,
-    from: ['dist/', 'docs/'],
-    to: '',
-  });
+    const files = ['.gitignore'].map((f) => resolve(__dirname, '..', f));
+    replace.sync({
+        files,
+        from: ['dist/', 'docs/'],
+        to: '',
+    });
 }
 
 /**
  * Calls any external programs to finish setting up the library
  */
 function finalize() {
-  console.log(underline.white('Finalizing'));
+    console.log(underline.white('Finalizing'));
 
-  // Recreate Git folder
-  const gitInitOutput = execSync(
-    'git init "' + resolve(__dirname, '..') + '"',
-    {}
-  ).toString();
-  console.log(green(gitInitOutput.replace(/(\n|\r)+/g, '')));
+    // Recreate Git folder
+    const gitInitOutput = execSync(
+        'git init "' + resolve(__dirname, '..') + '"',
+        {}
+    ).toString();
+    console.log(green(gitInitOutput.replace(/(\n|\r)+/g, '')));
 
-  // Remove post-install command
-  const jsonPackage = resolve(__dirname, '..', 'package.json');
-  const pkg = JSON.parse(readFileSync(jsonPackage) as any);
+    // Remove post-install command
+    const jsonPackage = resolve(__dirname, '..', 'package.json');
+    const pkg = JSON.parse(readFileSync(jsonPackage) as any);
 
-  // Note: Add items to remove from the package file here
-  delete pkg.scripts.postinstall;
+    // Note: Add items to remove from the package file here
+    delete pkg.scripts.postinstall;
 
-  // remove the dependencies that are required for the bootstrapping.
-  ['colors', 'prompt', 'replace-in-file', 'ts-node'].forEach((dep) => {
-    delete pkg.devDependencies[dep];
-  });
-  
-  pkg.version = "0.1.0";
+    // remove the dependencies that are required for the bootstrapping.
+    ['colors', 'prompt', 'replace-in-file', 'ts-node'].forEach((dep) => {
+        delete pkg.devDependencies[dep];
+    });
 
-  writeFileSync(jsonPackage, JSON.stringify(pkg, null, 2));
-  console.log(green('Postinstall script has been removed'));
+    pkg.version = "0.1.0";
 
-  console.log(yellow('Removing yarn.lock and performing a clean install...'));
-  rmSync('yarn.lock');
-  deleteFolderRecursive('node_modules');
-  execSync('yarn install');
-  execSync('yarn build');
-  execSync("git add . && git commit -am 'chore: init' --no-verify");
-  console.log('\n');
+    writeFileSync(jsonPackage, JSON.stringify(pkg, null, 2));
+    console.log(green('Postinstall script has been removed'));
+
+    console.log(yellow('Removing yarn.lock and performing a clean install...'));
+    rmSync('yarn.lock');
+    deleteFolderRecursive('node_modules');
+    execSync('yarn install');
+    execSync('yarn build');
+    execSync("git add . && git commit -am 'chore: init' --no-verify");
+    console.log('\n');
 }
 /**
  * Calls all of the functions needed to setup the library
@@ -166,37 +166,37 @@ function finalize() {
  * @param libraryName
  */
 function setupLibrary(libraryName: string, generateExample = false) {
-  console.log(
-    cyan(
-      '\nThanks for the info. The last few changes are being made... hang tight!\n\n'
-    )
-  );
-
-  // Get the Git username and email before the .git directory is removed
-  const username = execSync('git config user.name').toString().trim();
-  const usermail = execSync('git config user.email').toString().trim();
-
-  if (generateExample) {
-    yellow('Installing the test application into example/ directory...');
-    execSync('npx create-react-app example');
-    execSync('echo "SKIP_PREFLIGHT_CHECK=true" >> example/.env');
-  }
-  execSync('mv tools/README.md README.md');
-  removeItems();
-
-  modifyGitignoreFile();
-  modifyContents(libraryName, username, usermail);
-
-  if (generateExample) {
-    console.log(yellow('Linking packages to the example app...'));
-    execSync(
-      'cd example && yarn add link:.. link:../node_modules/react link:../node_modules/react-dom && cd ..'
+    console.log(
+        cyan(
+            '\nThanks for the info. The last few changes are being made... hang tight!\n\n'
+        )
     );
-  }
 
-  finalize();
+    // Get the Git username and email before the .git directory is removed
+    const username = execSync('git config user.name').toString().trim();
+    const usermail = execSync('git config user.email').toString().trim();
 
-  console.log(cyan("OK, you're all set. Happy coding!! ;)\n"));
+    if (generateExample) {
+        yellow('Installing the test application into example/ directory...');
+        execSync('npx create-react-app react-route-locker-engine --template typescript');
+        execSync('echo "SKIP_PREFLIGHT_CHECK=true" >> react-route-locker-engine/.env');
+    }
+    execSync('mv tools/README.md README.md');
+    removeItems();
+
+    modifyGitignoreFile();
+    modifyContents(libraryName, username, usermail);
+
+    if (generateExample) {
+        console.log(yellow('Linking packages to the react-route-locker-engine app...'));
+        execSync(
+            'cd ./react-route-locker-engine && yarn add link:.. link:../node_modules/react link:../node_modules/react-dom && cd ..'
+        );
+    }
+
+    finalize();
+
+    console.log(cyan("OK, you're all set. Happy coding!! ;)\n"));
 }
 /**
  * The library name is suggested by looking at the directory name of the
@@ -208,53 +208,53 @@ function setupLibrary(libraryName: string, generateExample = false) {
  * lowercased and returned
  */
 function libraryNameSuggested() {
-  return basename(resolve(__dirname, '..'))
-    .replace(/[^\w\d]|_/g, '-')
-    .replace(/^-+|-+$/g, '')
-    .toLowerCase();
+    return basename(resolve(__dirname, '..'))
+        .replace(/[^\w\d]|_/g, '-')
+        .replace(/^-+|-+$/g, '')
+        .toLowerCase();
 }
 
 const _promptSchemaLibraryName = {
-  properties: {
-    library: {
-      description: cyan(
-        'What do you want the library to be called? (use kebab-case)'
-      ),
-      pattern: /^[a-z]+(-[a-z]+)*$/,
-      type: 'string',
-      required: true,
-      message:
-        '"kebab-case" uses lowercase letters, and hyphens for any punctuation',
+    properties: {
+        library: {
+            description: cyan(
+                'What do you want the library to be called? (use kebab-case)'
+            ),
+            pattern: /^[a-z]+(-[a-z]+)*$/,
+            type: 'string',
+            required: true,
+            message:
+                '"kebab-case" uses lowercase letters, and hyphens for any punctuation',
+        },
     },
-  },
 };
 
 const _promptSchemaLibrarySuggest = {
-  properties: {
-    useSuggestedName: {
-      description: cyan(
-        'Would you like it to be called "' + libraryNameSuggested() + '"? [y/n]'
-      ),
-      pattern: /^(y(es)?|n(o)?)$/i,
-      type: 'string',
-      required: true,
-      message: 'You need to type "[y]es" or "[n]o" to continue...',
+    properties: {
+        useSuggestedName: {
+            description: cyan(
+                'Would you like it to be called "' + libraryNameSuggested() + '"? [y/n]'
+            ),
+            pattern: /^(y(es)?|n(o)?)$/i,
+            type: 'string',
+            required: true,
+            message: 'You need to type "[y]es" or "[n]o" to continue...',
+        },
     },
-  },
 };
 
 const _promptInstallExampleApp = {
-  properties: {
-    installExampleApp: {
-      description: yellow(
-        'Would you like to generate an example react app to test your library/component? [y/n]'
-      ),
-      pattern: /^(y(es)?|n(o)?)$/i,
-      type: 'string',
-      required: true,
-      message: 'You need to type "[y]es" or "[n]o" to continue...',
+    properties: {
+        installExampleApp: {
+            description: yellow(
+                'Would you like to generate an example react app to test your library/component? [y/n]'
+            ),
+            pattern: /^(y(es)?|n(o)?)$/i,
+            type: 'string',
+            required: true,
+            message: 'You need to type "[y]es" or "[n]o" to continue...',
+        },
     },
-  },
 };
 
 /**
@@ -262,22 +262,22 @@ const _promptInstallExampleApp = {
  * default directory, or if they want a different name to the one suggested
  */
 function libraryNameCreate() {
-  _prompt.get(_promptSchemaLibraryName, (err: any, res: any) => {
-    if (err) {
-      console.log(red('Sorry, there was an error building the workspace :('));
-      removeItems();
-      process.exit(1);
-      return;
-    }
-    _prompt.get(_promptInstallExampleApp, (err: any, res: any) => {
-      let installExampleApp = false;
-      if (err) return;
-      if (res.installExampleApp.toLowerCase().charAt(0) === 'y') {
-        installExampleApp = true;
-      }
-      setupLibrary(res.library, installExampleApp);
+    _prompt.get(_promptSchemaLibraryName, (err: any, res: any) => {
+        if (err) {
+            console.log(red('Sorry, there was an error building the workspace :('));
+            removeItems();
+            process.exit(1);
+            return;
+        }
+        _prompt.get(_promptInstallExampleApp, (err: any, res: any) => {
+            let installExampleApp = false;
+            if (err) return;
+            if (res.installExampleApp.toLowerCase().charAt(0) === 'y') {
+                installExampleApp = true;
+            }
+            setupLibrary(res.library, installExampleApp);
+        });
     });
-  });
 }
 
 /**
@@ -285,36 +285,36 @@ function libraryNameCreate() {
  * has been cloned into a custom directory (i.e. it's not 'typescript-library-starter')
  */
 function libraryNameSuggestedAccept() {
-  _prompt.get(_promptSchemaLibrarySuggest, (err: any, res: any) => {
-    if (err) {
-      console.log(red("Sorry, you'll need to type the library name"));
-      libraryNameCreate();
-    }
-
-    if (res.useSuggestedName.toLowerCase().charAt(0) === 'y') {
-      _prompt.get(_promptInstallExampleApp, (err: any, res: any) => {
-        let installExampleApp = false;
-        if (err) return;
-        if (res.installExampleApp.toLowerCase().charAt(0) === 'y') {
-          installExampleApp = true;
+    _prompt.get(_promptSchemaLibrarySuggest, (err: any, res: any) => {
+        if (err) {
+            console.log(red("Sorry, you'll need to type the library name"));
+            libraryNameCreate();
         }
-        setupLibrary(libraryNameSuggested(), installExampleApp);
-      });
-    } else {
-      libraryNameCreate();
-    }
-  });
+
+        if (res.useSuggestedName.toLowerCase().charAt(0) === 'y') {
+            _prompt.get(_promptInstallExampleApp, (err: any, res: any) => {
+                let installExampleApp = false;
+                if (err) return;
+                if (res.installExampleApp.toLowerCase().charAt(0) === 'y') {
+                    installExampleApp = true;
+                }
+                setupLibrary(libraryNameSuggested(), installExampleApp);
+            });
+        } else {
+            libraryNameCreate();
+        }
+    });
 }
 
 /**
  * Checks if the suggested library name is the default, which is 'typescript-library-starter'
  */
 function libraryNameSuggestedIsDefault() {
-  if (libraryNameSuggested() === 'typescript-library-starter') {
-    return true;
-  }
+    if (libraryNameSuggested() === 'typescript-library-starter') {
+        return true;
+    }
 
-  return false;
+    return false;
 }
 
 _prompt.start();
@@ -325,28 +325,28 @@ _prompt.message = '';
 process.stdout.write('\x1B[2J\x1B[0f');
 
 try {
-  execSync('git --version');
+    execSync('git --version');
 } catch {
-  console.log(red('Sorry, this script requires git'));
-  removeItems();
-  process.exit(1);
+    console.log(red('Sorry, this script requires git'));
+    removeItems();
+    process.exit(1);
 }
 
 // Say hi!
 console.log(
-  cyan(
-    "Hi! You're almost ready to create the next React Library with Typescript."
-  )
+    cyan(
+        "Hi! You're almost ready to create the next React Library with Typescript."
+    )
 );
 
 // Generate the library name and start the tasks
 if (process.env.CI == null) {
-  if (!libraryNameSuggestedIsDefault()) {
-    libraryNameSuggestedAccept();
-  } else {
-    libraryNameCreate();
-  }
+    if (!libraryNameSuggestedIsDefault()) {
+        libraryNameSuggestedAccept();
+    } else {
+        libraryNameCreate();
+    }
 } else {
-  // This is being run in a CI environment, so don't ask any questions
-  setupLibrary(libraryNameSuggested());
+    // This is being run in a CI environment, so don't ask any questions
+    setupLibrary(libraryNameSuggested());
 }
